a5b506b989be9dd7920fbdc2f3a5a225f3d2458e
BriiB <anton.heidemann.te15c@gmail.com>
2017-11-08 13:54:00 +0100

    Skapat en application i Eclipse

---
+++ src/Circle/main.java
@@ -0,0 +1,22 @@
+package Circle;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class main extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		Group root = new Group();
+		Scene scene = new Scene(root, 500, 500);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
