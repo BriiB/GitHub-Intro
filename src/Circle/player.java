a4aaddbc213e19929793ceeb18f81d8f9be2fabb
BriiB <anton.heidemann.te15c@gmail.com>
2017-11-29 13:41:11 +0100

    Skapade en constructor, och gjorde en större storlek

--- src/Circle/player.java
+++ src/Circle/player.java
@@ -1,11 +1,15 @@
 package Circle;
 
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class player extends Circle{
 	
-	public player(){
+	public player(double radius){
+		
+		this.setRadius(radius);
+		
+		this.setFill(Color.rgb((int)(Math.random()*255), (int)(Math.random()*255), (int)(Math.random()*255)));
 		
 	}
-
 }
