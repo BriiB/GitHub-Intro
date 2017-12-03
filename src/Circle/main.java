e33d6c20cf04034671216ed230700e392a1b54d4
BriiB <anton.heidemann.te15c@gmail.com>
2017-12-03 22:32:09 +0100

    Skapade ny rörelse med hjälp utav ArrayList och animationTimer

--- src/Circle/main.java
+++ src/Circle/main.java
@@ -1,66 +1,94 @@
 package Circle;
 
+import java.util.ArrayList;
+
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class main extends Application{
-	
+
 	public static final double WORLD_WIDTH = 500;
 	public static final double WORLD_HEIGHT = 500;
-	
+
 	Group root = new Group();
 	Scene scene = new Scene(root, WORLD_WIDTH, WORLD_HEIGHT);
-	
+
 	//private Circle cir = new Circle(WORLD_WIDTH/50);
-	private player player = new player(WORLD_WIDTH/50);
 	
+	private double playerRadius = (((WORLD_WIDTH+WORLD_HEIGHT)/2)/30);
+	private player player = new player(playerRadius);
+	private ArrayList<KeyCode> keys = new ArrayList<KeyCode>();
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
 		createCircle();
 		eventListener();
-		
+
 		primaryStage.setTitle("Circle");
 		primaryStage.setScene(scene);
 		primaryStage.setResizable(false);
 		primaryStage.show();
-		
+
 	}
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	private void createCircle(){
-		Circle cir = new Circle(WORLD_WIDTH/50);
-		
+
 		//cir.setFill(Color.rgb(0, 255, 255));
-		cir.setTranslateX(WORLD_WIDTH/2);
-		cir.setTranslateY(WORLD_WIDTH/2);
-		
-		root.getChildren().addAll(cir);
+		player.setTranslateX(WORLD_WIDTH/2);
+		player.setTranslateY(WORLD_WIDTH/2);
+
+		root.getChildren().addAll(player);
+
+	}
+
+	private void eventListener() {
+		scene.setOnKeyPressed(event->{
+			if(!keys.contains(event.getCode())){
+				keys.add(event.getCode());
+			}});
+
+		scene.setOnKeyReleased(event->{
+			if(keys.contains(event.getCode())){
+				keys.remove(event.getCode());
+			}
+		});
+
+
+		AnimationTimer at = new AnimationTimer() {
+
+			double moveTimer = 0;
+
+			@Override
+			public void handle(long now) {
+
+				if(now - moveTimer >= 1000000) {
+					moveTimer = now;
+					if(keys.contains(KeyCode.W)) {
+						player.setTranslateY(player.getTranslateY()-(playerRadius/5));
+					}
+					if(keys.contains(KeyCode.A)) {
+						player.setTranslateX(player.getTranslateX()-(playerRadius/5));
+					}
+					if(keys.contains(KeyCode.S)) {
+						player.setTranslateY(player.getTranslateY()+(playerRadius/5));
+					}
+					if(keys.contains(KeyCode.D)) {
+						player.setTranslateX(player.getTranslateX()+(playerRadius/5));
+					}
+				}
+			}
+		};	
+		at.start();
 		
 	}
-	
-	private void eventListener() {
-		 		scene.setOnKeyPressed(event->{
-		 			if(event.getCode() == KeyCode.W) {
-		 				player.setTranslateY(player.getTranslateY()-5);
-		 			}
-		 			if(event.getCode() == KeyCode.A) {
-		 				player.setTranslateX(player.getTranslateX()-5);
-		 			}
-		 			if(event.getCode() == KeyCode.S) {
-		 				player.setTranslateY(player.getTranslateY()+5);
-		 			}
-		 			if(event.getCode() == KeyCode.D) {
-		 				player.setTranslateX(player.getTranslateX()+5);
-		 			}
-		 		});
-		 	}
 	
 }
